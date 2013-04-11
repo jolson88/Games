c719e0ed96a7b466310518e9db242fc396027903
Jason Olson <jolson88@outlook.com>
2013-04-10 22:37:43 -0700

    Changed Acorn to MonoGame
    Backed up GMS project into AcornGmx folder.

---
+++ Acorn/Acorn/GamePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using MonoGame.Framework;
+
+
+namespace Acorn
+{
+    /// <summary>
+    /// The root page used to display the game.
+    /// </summary>
+    public sealed partial class GamePage : SwapChainBackgroundPanel
+    {
+        readonly AcornGame _game;
+
+        public GamePage(string launchArguments)
+        {
+            this.InitializeComponent();
+
+            // Create the game.
+            _game = XamlGame<AcornGame>.Create(launchArguments, Window.Current.CoreWindow, this);
+        }
+    }
+}
---
+++ Jarrett/Sprite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Jarrett
+{
+    public class Sprite
+    {
+        public Texture2D Texture { get; set; }
+
+        public Sprite(Texture2D texture)
+        {
+            this.Texture = texture;
+        }
+    }
+}
