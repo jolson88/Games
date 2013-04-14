c92a9f27c6cb15799f150c87a3867b16981d9268
Harry Pierson <harry@devhawk.net>
2013-04-14 16:39:50 -0700

    Added basic movement and bouncing of screen edge behavior

---
+++ FunPhysics/FunPhysics/Behaviors/BounceOffScreenEdgeBehavior.cs
@@ -0,0 +1,36 @@
+﻿using Hiromi;
+using Hiromi.Behaviors;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunPhysics.Behaviors
+{
+    class BounceOffScreenEdgeBehavior : GameObjectBehavior
+    {
+        public override void Update(GameTime gameTime)
+        {
+            var view_bounds = GraphicsService.Instance.GraphicsDevice.Viewport.Bounds;
+
+            //TODO: Take center into account
+            //var c = GameObject.Sprite.Center;
+            var sprite_bounds = GameObject.Sprite.Texture.Bounds;
+            sprite_bounds.Offset(
+                (int)(GameObject.Position.X * view_bounds.Width),
+                (int)(GameObject.Position.Y * view_bounds.Height));
+
+            var move_behavior = this.GameObject.GetBehavior<MovementBehavior>();
+
+            if (sprite_bounds.Left < view_bounds.Left || sprite_bounds.Right > view_bounds.Right)
+                move_behavior.Velocity = new Vector2(move_behavior.Velocity.X * -1, move_behavior.Velocity.Y);
+
+            if (sprite_bounds.Top < view_bounds.Top || sprite_bounds.Bottom > view_bounds.Bottom)
+                move_behavior.Velocity = new Vector2(move_behavior.Velocity.X, move_behavior.Velocity.Y * -1);
+        }
+    }
+
+
+}
--- FunPhysics/FunPhysics/Screens/MainScreen.cs
+++ FunPhysics/FunPhysics/Screens/MainScreen.cs
@@ -1,4 +1,7 @@
-﻿using Hiromi;
+﻿using FunPhysics.Behaviors;
+using Hiromi;
+using Hiromi.Behaviors;
+using Hiromi.Processing;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -16,8 +19,10 @@ namespace FunPhysics.Screens
             var biohazard = new GameObject()
             {
                 Sprite = new Sprite(ContentService.Instance.Content.Load<Texture2D>("Biohazard")),
-                Position = new Vector2(0.2f, 0.1f),
+                Position = new Vector2(0.1f, 0.1f),
             };
+            biohazard.AddBehavior(new MovementBehavior(new Vector2(0.4f, 0.2f)));
+            biohazard.AddBehavior(new BounceOffScreenEdgeBehavior());
 
             GameObjectService.Instance.AddGameObject(biohazard);
         }
