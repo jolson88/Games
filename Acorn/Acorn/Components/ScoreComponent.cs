2b09728f89ab668568a6aa420f90e7ef7ef99170
Jason Olson <jolson88@outlook.com>
2013-04-20 00:50:06 -0700

    Scoring impl'd. Removed old projects.

---
+++ Acorn/Acorn/Components/ScoreComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hiromi;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Acorn.Components
+{
+    public class ScoreComponent : IComponent
+    {
+        public int PlayerIndex { get; set; }
+        public Texture2D Fill { get; set; }
+
+        public ScoreComponent(int playerIndex, Color fillColor)
+        {
+            this.PlayerIndex = playerIndex;
+            this.Fill = new Texture2D(GraphicsService.Instance.GraphicsDevice, 1, 1);
+            this.Fill.SetData(new Color[] { fillColor });
+        }
+    }
+}
