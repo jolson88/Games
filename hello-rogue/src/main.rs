89a2606f8cc5f1618c2e9e97cca147d2dc99121e
Jason Olson <jolson88@outlook.com>
2018-11-06 22:51:45 -0800

    Part 2
    https://tomassedovic.github.io/roguelike-tutorial/part-2-object-map.html

--- hello-rogue/src/main.rs
+++ hello-rogue/src/main.rs
@@ -4,9 +4,15 @@ use tcod::console::*;
 use tcod::Color;
 use tcod::colors;
 
+type Map = Vec<Vec<Tile>>;
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+const LIMIT_FPS: i32 = 20;
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
-const LIMIT_FPS: i32 = 20;
 
 fn main() {
     let mut root = Root::initializer()
@@ -17,18 +23,15 @@ fn main() {
         .renderer(Renderer::GLSL)
         .init();
     tcod::system::set_fps(LIMIT_FPS);
-    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
     let mut objects = [player, npc];
+    let map = make_map();
 
     while !root.window_closed() {
-        for object in &objects {
-            object.draw(&mut con);
-        }
-
-        blit(&mut con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
+        render_all(&mut root, &mut con, &objects, &map);
         root.flush();
 
         for object in &objects {
@@ -36,29 +39,56 @@ fn main() {
         }
 
         let player = &mut objects[0];
-        let exit = handle_keys(&mut root, player);
+        let exit = handle_keys(&mut root, player, &map);
         if exit {
             break
         }
     }
 }
 
-fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
     let key = root.wait_for_keypress(true);
     match key {
         Key { code: Escape, .. } => return true,
-        Key { code: Up, .. } => player.move_by(0, -1),
-        Key { code: Down, .. } => player.move_by(0, 1),
-        Key { code: Left, .. } => player.move_by(-1, 0),
-        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Up, .. } => player.move_by(0, -1, map),
+        Key { code: Down, .. } => player.move_by(0, 1, map),
+        Key { code: Left, .. } => player.move_by(-1, 0, map),
+        Key { code: Right, .. } => player.move_by(1, 0, map),
         _ => {},
     }
     false
 }
 
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    for object in objects {
+        object.draw(con);
+    }
+
+    blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
+}
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+    map
+}
+
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
@@ -76,9 +106,11 @@ impl Object {
         }
     }
 
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
+        if !map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     pub fn draw(&self, con: &mut Console) {
@@ -88,5 +120,21 @@ impl Object {
 
     pub fn clear(&self, con: &mut Console) {
         con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile { blocked: false, block_sight: false }
+    }
+
+    pub fn wall() -> Self {
+        Tile { blocked: true, block_sight: true }
     }
 }
